51947e31b50f6ef0ef1cb935cda25ec717acdecd
Sounder Rajendran <sounderajendran@gmail.com>
2021-08-29 14:17:56 +0530

    functionality of 'about page' popup

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var currentItemIndex = 1;
 var quesNum = document.getElementById("quesNum");
 var contentImg = document.getElementById("contentImg");
 var folderName = ""; 
-
-//var select = document.getElementById('language');
-//var option = select.options[select.selectedIndex];
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
 //Probability, ETS_Quants_MixedSet2,ETS_Quants_MixedSet3...
 
@@ -103,4 +103,21 @@ function startTest() {
 	    document.getElementById('next-btn').style.display = 'none';
         }
     }
+}
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
